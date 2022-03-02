a65ba052adfb6a7821456d08c2ab4d4776a875c5
wiss <73744299+wissem18@users.noreply.github.com>
2022-03-02 21:29:11 +0100

    Update script.js

--- exercice2/script.js
+++ exercice2/script.js
@@ -1,22 +1,12 @@
-var x=document.getElementById("x1");
-var y=document.getElementById("x2");
-var z=document.getElementById("x3");
-var s=["blue","red","yellow","black","grey","cyan","purple","pink"];
-x.addEventListener("click",()=>{
-    let i=Math.random()*7;
-    i=Math.floor(i);
-    x.style.color=s[i];
-}
-)
-y.addEventListener("click",()=>{
-    let i=Math.random()*7;
-    i=Math.floor(i);
-    y.style.color=s[i];
-}
-)
-z.addEventListener("click",()=>{
-    let i=Math.random()*7;
-    i=Math.floor(i);
-    z.style.color=s[i];
-}
-)
+var x = document.getElementById("x1");
+var y = document.getElementById("x2");
+var z = document.getElementById("x3");
+x.addEventListener("click", () => {
+    x.style.color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+})
+y.addEventListener("click", () => {
+    y.style.color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+})
+z.addEventListener("click", () => {
+    z.style.color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+})
